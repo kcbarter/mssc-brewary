345d778f493655c2431523ba72b02b081c8860f3
kcbar <Kbarter@mail.greenriver.edu>
2020-03-31 18:10:00 -0700

    Intial commit for the project

---
+++ src/main/java/guru/springFramework/msscbrewary/MsscBrewaryApplication.java
@@ -0,0 +1,13 @@
+package guru.springFramework.msscbrewary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springFramework/msscbrewary/MsscBrewaryApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springFramework.msscbrewary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
