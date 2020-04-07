efe3faa07284ed3da94242cccc614127c37d5612
kcbar <Kbarter@mail.greenriver.edu>
2020-04-06 18:18:30 -0700

    Created a PostMapping route in Beer controller to save a new beer.

--- src/main/java/guru/springFramework/msscbrewary/web/controller/BeerController.java
+++ src/main/java/guru/springFramework/msscbrewary/web/controller/BeerController.java
@@ -2,10 +2,12 @@ package guru.springFramework.msscbrewary.web.controller;
 
 import guru.springFramework.msscbrewary.services.BeerService;
 import java.util.UUID;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import guru.springFramework.msscbrewary.web.model.BeerDto;
@@ -23,5 +25,16 @@ public class BeerController {
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto){
+
+        BeerDto saveDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + saveDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
